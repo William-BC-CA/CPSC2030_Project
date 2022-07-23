44f802b9cf5fd5df0784ae46739e9e93dccbe4b0
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-07-23 07:46:55 +0000

    create reviews php and validation!

---
+++ subpages/reviews.php
@@ -0,0 +1,102 @@
+<?php
+  // error reporting
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+
+  // Import functions
+  require_once('validate-review.php');
+
+  the_validation_system();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/main-styles.css">
+    <link rel="icon" href="../img/game.ico">
+    <title>NTRX Counter-Strike Modding Hub</title>
+</head>
+<body>
+    <div class = "wrapper">
+        <header>
+            <h1><span class = "first-word">NTRX</span> Counter-Strike Modding Hub</h1>
+        </header>
+        <nav>
+            <ul>
+                <a href = "../"><li>Home</li></a>
+                <a href = "maps.html"><li>Maps</li></a>
+                <li>Skins
+                    <ul>
+                        <a href = "weapons.html"><li>Weapons</li></a>
+                        <a href = "envtextures.html"><li>Environment Textures</li></a>
+                    </ul>
+                </li>
+                <a href = "sounds.html"><li>Sounds</li></a>
+                <li>Misc.
+                    <ul>
+                        <a href = "plugins.html"><li>Plugins</li></a>
+                        <a href = "submitmod.php"><li>Submit Mod</li></a>
+                        <a href = "reviews.php"><li>Reviews</li></a>
+                        <a href = "tools.html"><li>Tools</li></a>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <main>
+            <form action="reviews.php" method = "post">
+                <label for="firstName">First Name: </label>
+                <input type="text" name = "firstName" id = "firstName">
+                <!-- firstName Message -->
+                <?php print_validate_message("firstName") ?>
+                <label for = "lastName">Last Name: </label>
+                <input type = "text" name = "lastName" id = "lastName">
+                <!-- lastName Message -->
+                <?php print_validate_message("lastName") ?>
+                <label for = "email">Email: </label>
+                <input type = "text" name = "email" id = "email">
+                <!-- Email Message -->
+                <?php print_validate_message("email") ?>
+                <label for = "comments">Comment: (recommended, but optional)</label>
+                <textarea name="comments" id="comments" cols="30" rows="10"></textarea>
+                <fieldset>
+                    <legend>Rating:</legend>
+                    <input type = "radio" id = "1" name = "ratings[]">
+                        <label for = "1">1</label>
+                    <input type = "radio" id = "2" name = "ratings[]">
+                        <label for = "2">2</label>
+                    <input type = "radio" id = "3" name = "ratings[]">
+                        <label for = "3">3</label>
+                    <input type= "radio" id = "4" name = "ratings[]">
+                        <label for = "4">4</label>
+                    <input type = "radio" id = "5" name = "ratings[]">
+                        <label for = "5">5</label>
+                    <input type = "radio" id = "6" name = "ratings[]">
+                        <label for = "6">6</label>
+                    <input type = "radio" id = "7" name "rating[]">
+                        <label for = "7">7</label>
+                    <input type = "radio" id = "8" name "rating[]">
+                        <label for = "8">8</label>
+                    <input type = "radio" id = "9" name "rating[]">
+                        <label for = "9">9</label>
+                    <input type = "radio" id = "10" name "rating[]">
+                        <label for = "10">10</label>
+
+                    <!-- DISPLAY MESSAGE -->
+                    <?php print_validate_message("ratings") ?>
+                </fieldset>
+                <label for = "age">Age: </label>
+                <input type = "text" name = "age" id = "age">
+                <?php print_validate_message("age") ?>
+                <?php printFinal() ?>
+                <input type = "submit" value = "Submit">
+            </form>
+        </main>
+        <footer>
+            <p>Copyright &copy; 2022 NOTORIEX LEGACY. All Rights Reserved.</p>
+        </footer>
+</body>
+</html>
---
+++ subpages/validate-review.php
@@ -0,0 +1,137 @@
+<?php
+    $valid = false;
+    $val_messages = Array();
+
+    // VARIABLES
+    $firstName = $lastName = $email = $rating = $age = "";
+
+    function printFinal(){
+        global $valid;
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            if ($valid == true){
+                echo "<div class = 'results'>Your review has been submitted!</div>";
+            }
+        }
+    }
+
+    function the_validation_system(){
+        global $firstName;
+        global $lastName;
+        global $email;
+        // global $comments;
+        global $rating;
+        global $age;
+        global $valid;
+        global $val_messages;
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            // SET VARIABLES
+            $firstName = $_POST["firstName"];
+            $lastName = $_POST["lastName"];
+            $email = $_POST["email"];
+            // $comments = $_POST["comments"];
+            $age = $_POST["age"];
+            $firstNameChecker = $lastNameChecker = $ageChecker = true;
+            
+            if (isset($firstName) && isset($lastName) && isset($email) && isset($age)){
+                foreach($_POST as $type => $value){
+                    if ($type == 'firstName'){
+                        for($i = 0; $i < strlen($firstName); $i++){
+                            $letter = substr($firstName, $i, $i + 1);
+                            if (($letter >= 'A' && $letter <= 'Z') || ($letter >= 'a' && $letter <= 'z')){
+                                $val_messages = "";
+                            }
+                            else {
+                                $val_messages = "Insert bruh moment. Dude your name has to be in letters not numbers! Are you a robot?";
+                                $i = strlen($firstName) - 1;
+                                $firstNameChecker = false;
+                            }
+                        }
+                        if ($firstNameChecker == "true"){
+                            if (substr($firstName, 0, 1) >= 'A' && substr($firstName, 0, 1) <= 'Z'){
+                                $val_messages = "";
+                            }
+                            else {
+                                $val_messages = "First letter must be capital!";
+                            }
+                        }
+
+                        // if (strlen($firstName) > 2){
+                        //     $val_messages = "";
+                        // }
+                        // else {
+                        //     $val_messages = "First Name must have more than 2 letters!";
+                        //     break;
+                        // }
+                    }
+                    if ($type == "lastName"){
+                        for($i = 0; $i < strlen($lastName); $i++){
+                            $letter = substr($lastName, $i, $i + 1);
+                            if (($letter >= 'A' && $letter <= 'Z') || ($letter >= 'a' && $letter <= 'z')){
+                                $val_messages["lastName"] = "";
+                            }
+                            else {
+                                $val_messages["lastName"] = "Insert bruh moment. Dude your name has to be in letters not numbers! Are you a robot?";
+                                $i = strlen($lastName) - 1;
+                                $lastNameChecker = false;
+                            }
+                        }
+                        if ($lastNameChecker == true){
+                            if (substr($lastName, 0, 1) >= 'A' && substr($lastName, 0, 1) <= 'Z'){
+                                $val_messages["lastName"] = "";
+                            }
+                            else {
+                                $val_messages["lastName"] = "First letter must be capital!";
+                            }
+                        }
+
+                        // if (strlen($lastName) > 2){
+                        //     $val_messages = "";
+                        // }
+                        // else {
+                        //     $val_messages = "Last Name must have more than 2 letters!";
+                        //     break;
+                        // }
+                    }
+                    if ($type = "email"){
+                        // Courtesy of Adnan Reza (Prof)
+                        $eChecker= '#^(.+)@([^\.].*)\.([a-z]{2,})$#';
+                        if (preg_match($eChecker, $email)){
+                            $val_messages["email"] = "";
+                        }
+                        else {
+                            $val_messages["email"] = "Invalid Email!";
+                        }
+                    }
+                }
+            }
+            if ((isset($_POST["ratings"])) && (count($_POST["ratings"]) == 1)){
+                $val_messages["ratings"] = "";
+            }
+            else {
+                $val_messages["ratings"] = "You must choose a rating!";
+            }
+            foreach($val_messages as $value){
+                if ($value != ""){
+                    return;
+                }
+            }
+    
+            $valid = true;
+        }
+    }
+
+    function print_validate_message($type){
+        global $val_messages;
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+            if ((isset($val_messages[$type])) || ($type == "ratings" && isset ($_POST["ratings"]))){
+                $toPrint = $val_messages[$type];
+                echo "<p class = 'failure-message'>";
+                echo $toPrint;
+                echo "</p>";
+            }
+        }
+    }
+?>
