1bf2e0a102ded1056a2a64b65aaecd3c4067d198
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-08-01 21:21:25 +0000

    attempt fix mod post validation

--- subpages/modpost.php
+++ subpages/modpost.php
@@ -2,7 +2,8 @@
 
     function receive_message(){
         if ($_SERVER["REQUEST_METHOD"] == "POST"){
-            if ((isset($_FILES["fileName"])) && (!empty($_FILES["fileName"]["name"]))){
+            //  && (!empty($_FILES["fileName"]["name"]))
+            if ((isset($_FILES["fileName"]))){
                 echo "<p class = 'results'>File submitted!</p>";
             }
             else {
