fa249d542f8a9af2a1c356531dec149b6f34545b
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-08-01 21:43:21 +0000

    removed mod post validation, will repair later

--- subpages/modpost.php
+++ subpages/modpost.php
@@ -3,12 +3,12 @@
     function receive_message(){
         if ($_SERVER["REQUEST_METHOD"] == "POST"){
             //  && (!empty($_FILES["fileName"]["name"]))
-            if ((is_uploaded_file($_FILES["myFile"]["tmp_name"]))){
+            // if ((is_uploaded_file($_FILES["myFile"]["tmp_name"]))){
                 echo "<p class = 'results'>File submitted!</p>";
-            }
-            else {
-                echo "<p class = 'failure-message'>Please choose a file!</p>";
-            }
+            // }
+            // else {
+                // echo "<p class = 'failure-message'>Please choose a file!</p>";
+            // }
         }
     }
 ?>
