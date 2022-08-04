b7c80e929a5868e158c0a94a8b28caf2280680a0
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-08-04 20:27:16 +0000

    decluttered config.php!

--- subpages/database/config.php
+++ subpages/database/config.php
@@ -1,5 +1,4 @@
 <?php
-    // mysql://bd4ed4f889f2d8:df248871@us-cdbr-east-06.cleardb.net/heroku_b5420f284a2ac9d?reconnect=true
     define('DBHOST', 'us-cdbr-east-06.cleardb.net');
     define('DBNAME', 'heroku_b5420f284a2ac9d');
     define('DBUSER', 'bd4ed4f889f2d8');
